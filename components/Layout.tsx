6888f94d9c6bd441a77ab5c3d6856ed1f64cb932
Nate Look <nate@natelook.com>
2022-05-05 17:43:18 -0700

    add meta and favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from './Footer';
+import Meta from './Meta';
 import Nav from './Nav';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <React.Fragment>
+    <Meta />
     <Nav />
     <div className='max-w-xl mx-auto mt-44'>{children}</div>
     <Footer />
